de86f72bb7acfd7304c0dc810b3ba3a8597cf2a9
GiverPlay007 <play.giver.007@gmail.com>
2021-04-14 21:20:40 -0300

    Fix Firebase initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:chat/views/signin.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:chat/views/signup.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
 
